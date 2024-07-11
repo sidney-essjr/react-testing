6aca861690bee6dde4e75a06b2f4898452c2c013
sidney.jr <sidney.e.s.s.jr@gmail.com>
2024-07-11 09:44:28 -0300

    adiciona um novo teste

--- src/components/application/Application.tsx
+++ src/components/application/Application.tsx
@@ -1,37 +1,40 @@
-export default function Application() {
+export default function Application({ loading }: { loading: boolean }) {
   return (
     <>
-    <h1>Job application form</h1>
-    <h2>Section 1</h2>
-    <form>
-      <div>
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name" />
-      </div>
-      <div>
-        <label htmlFor="bio">Bio</label>
-        <textarea id="bio" name="bio" />
-      </div>
-      <div>
-        <label htmlFor="job-location">Job location</label>
-        <select name="" id="job-locatino">
-          <option value="">Select a country</option>
-          <option value="US">United States</option>
-          <option value="GB">United kingdom</option>
-          <option value="CA">Canada</option>
-          <option value="IN">India</option>
-          <option value="AU">Australia</option>
-        </select>
-      </div>
+      <h1>Job application form</h1>
+      <h2>Section 1</h2>
+      {!loading && (
+        <form>
+          <div>
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" />
+          </div>
+          <div>
+            <label htmlFor="bio">Bio</label>
+            <textarea id="bio" name="bio" />
+          </div>
+          <div>
+            <label htmlFor="job-location">Job location</label>
+            <select name="" id="job-locatino">
+              <option value="">Select a country</option>
+              <option value="US">United States</option>
+              <option value="GB">United kingdom</option>
+              <option value="CA">Canada</option>
+              <option value="IN">India</option>
+              <option value="AU">Australia</option>
+            </select>
+          </div>
 
-      <div>
-        <label htmlFor="terms">
-          <input type="checkbox" id="terms" /> I agree to therms and conditions
-        </label>
-      </div>
+          <div>
+            <label htmlFor="terms">
+              <input type="checkbox" id="terms" /> I agree to therms and
+              conditions
+            </label>
+          </div>
 
-      <button>Submit</button>
-    </form>
+          <button>Submit</button>
+        </form>
+      )}
     </>
   );
 }
--- src/components/application/application.test.tsx
+++ src/components/application/application.test.tsx
@@ -4,42 +4,48 @@ import Application from "./Application";
 describe("Application", () => {
   describe("renders correctly", () => {
     it("should have rendered a heading level 1", () => {
-      render(<Application />);
+      render(<Application loading={false} />);
       const heading1 = screen.getByRole("heading", { level: 1 });
       expect(heading1).toBeInTheDocument();
     });
     it("should have rendered a heading level 2", () => {
-      render(<Application />);
+      render(<Application loading={false} />);
       const heading2 = screen.getByRole("heading", { level: 2 });
       expect(heading2).toBeInTheDocument();
     });
 
+    it("should not render the form", () => {
+      render(<Application loading={true} />);
+      const form = document.createElement("form")
+      expect(form).not.toBeInTheDocument();
+    });
+
     it("should have rendered a textbox with name 'Name'", () => {
-      render(<Application />);
+      render(<Application loading={false} />);
       const textbox = screen.getByRole("textbox", { name: "Name" });
       expect(textbox).toBeInTheDocument();
     });
 
     it("should have rendered a textbox with name 'Bio'", () => {
-      render(<Application />);
+      render(<Application loading={false} />);
       const textbox = screen.getByRole("textbox", { name: "Bio" });
       expect(textbox).toBeInTheDocument();
     });
 
     it("should have rendered a combobox", () => {
-      render(<Application />);
+      render(<Application loading={false} />);
       const combobox = screen.getByRole("combobox");
       expect(combobox).toBeInTheDocument();
     });
 
     it("should have rendered a checkbox", () => {
-      render(<Application />);
+      render(<Application loading={false} />);
       const checkbox = screen.getByRole("checkbox");
       expect(checkbox).toBeInTheDocument();
     });
 
     it("should have rendered a button", () => {
-      render(<Application />);
+      render(<Application loading={false} />);
       const button = screen.getByRole("button");
       expect(button).toBeInTheDocument();
     });
